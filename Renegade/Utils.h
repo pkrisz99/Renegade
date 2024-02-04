bea70739f214fa552a97b9c238a72a641c6e2b3f
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2024-02-04 09:01:51 +0100

    Net 13, trained with tapered WDL
    Score of Renegade dev vs Renegade latest: 863 - 737 - 1592  [0.520] 3192
    ...      Renegade dev playing White: 738 - 93 - 766  [0.702] 1597
    ...      Renegade dev playing Black: 125 - 644 - 826  [0.337] 1595
    ...      White vs Black: 1382 - 218 - 1592  [0.682] 3192
    Elo difference: 13.7 +/- 8.5, LOS: 99.9 %, DrawRatio: 49.9 %
    SPRT: llr 2.96 (100.7%), lbound -2.94, ubound 2.94 - H1 was accepted
    
    1.1.0 dev 4
    2038996 nodes 2010101 nps

--- Renegade/Utils.h
+++ Renegade/Utils.h
@@ -19,7 +19,7 @@ using std::endl;
 using std::get;
 using Clock = std::chrono::high_resolution_clock;
 
-constexpr std::string_view Version = "1.1.0 dev 3";
+constexpr std::string_view Version = "1.1.0 dev 4";
 
 // Evaluation helpers -----------------------------------------------------------------------------
 
---
+++ Renegade/renegade-net.rs
@@ -0,0 +1,42 @@
+/// Training parameters used to create Renegade's networks
+
+use bullet::{
+    inputs, outputs, Activation, LocalSettings, LrScheduler, TrainerBuilder, TrainingSchedule, WdlScheduler,
+};
+
+fn main() {
+    let mut trainer = TrainerBuilder::default()
+        .quantisations(&[181, 64])
+        .input(inputs::Chess768)
+        .output_buckets(outputs::Single)
+        .feature_transformer(512)
+        .activate(Activation::SCReLU)
+        .add_layer(1)
+        .build();  //trainer.load_from_checkpoint("checkpoints/testnet");
+
+    let schedule = TrainingSchedule {
+        net_id: "renegade-net-13".to_string(),
+        batch_size: 16384,
+        eval_scale: 400.0,
+        start_epoch: 1,
+        end_epoch: 25,
+        wdl_scheduler: WdlScheduler::Linear {
+            start: 0.2,
+            end: 0.5,
+        },
+        lr_scheduler: LrScheduler::Step {
+            start: 0.001,
+            gamma: 0.1,
+            step: 10,
+        },
+        save_rate: 1,
+    };
+
+    let settings = LocalSettings {
+        threads: 4,
+        data_file_paths: vec!["net-12-bulletformat"],
+        output_directory: "checkpoints",
+    };
+
+    trainer.run(&schedule, &settings);
+}
