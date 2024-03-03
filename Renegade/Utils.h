a2565dc2f588c628badae183a3bf539a5d640353
Krisztián Peőcz <5463243+pkrisz99@users.noreply.github.com>
2024-03-03 11:00:39 +0100

    Remove move ordering fallback when no history scores exist (#11)
    This removes the move ordering method that was used before the implementation of history heuristic. It was used as a fallback in the case of history score = 0 until now.
    
    The test takes the high score for the longest running SPRT so far, easily beating the previous record of 18k games:
    
    Elo   | -0.46 +- 2.21 (95%)
    SPRT  | 6.0+0.06s Threads=1 Hash=16MB
    LLR   | 2.96 (-2.94, 2.94) [-5.00, 0.00]
    Games | N: 43394 W: 9890 L: 9948 D: 23556
    Penta | [319, 5208, 10715, 5122, 333]
    
    1.1.0 dev 12
    Bench: 2582202

--- Renegade/Utils.h
+++ Renegade/Utils.h
@@ -19,7 +19,7 @@ using std::endl;
 using std::get;
 using Clock = std::chrono::high_resolution_clock;
 
-constexpr std::string_view Version = "1.1.0 dev 11";
+constexpr std::string_view Version = "1.1.0 dev 12";
 
 // Evaluation helpers -----------------------------------------------------------------------------
 
