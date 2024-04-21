9350de3a9bdfab152e4b8a39003986aa81f41e10
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2024-04-21 23:42:27 +0200

    Net 18 take 1
    Positions: 2.038.919.989
    Nodes: 3463013

--- Renegade/renegade-net.rs
+++ Renegade/renegade-net.rs
@@ -15,11 +15,11 @@ fn main() {
         .build();  //trainer.load_from_checkpoint("checkpoints/testnet");
 
     let schedule = TrainingSchedule {
-        net_id: "renegade-net-17".to_string(),
+        net_id: "renegade-net-18".to_string(),
         batch_size: 16384,
         eval_scale: 400.0,
         start_epoch: 1,
-        end_epoch: 25,
+        end_epoch: 23,
         wdl_scheduler: WdlScheduler::Linear {
             start: 0.3,
             end: 0.6,
@@ -27,14 +27,14 @@ fn main() {
         lr_scheduler: LrScheduler::Step {
             start: 0.001,
             gamma: 0.1,
-            step: 10,
+            step: 9,
         },
         save_rate: 1,
     };
 
     let settings = LocalSettings {
         threads: 4,
-        data_file_paths: vec!["20240221_20240325"],
+        data_file_paths: vec!["../nnue/data/20240221_20240325_20240421"],
         output_directory: "checkpoints",
     };
 
