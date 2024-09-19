599485cb687587016275b7de67c765c02afed279
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2024-09-19 18:21:02 +0300

    Datagen w/ book
    Bench: 3000453

--- Renegade/Neural.h
+++ Renegade/Neural.h
@@ -16,7 +16,7 @@
 
 // Network constants
 #ifndef NETWORK_NAME
-#define NETWORK_NAME "renegade-exp-normal-135.bin"
+#define NETWORK_NAME "renegade-exp-book-135.bin"
 #endif
 
 constexpr int FeatureSize = 768;
