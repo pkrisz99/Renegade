d6d0b8825beb9ffee5f350c0769f9234acdd6da1
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2025-01-25 18:05:37 +0100

    Bump version

--- Renegade/Utils.h
+++ Renegade/Utils.h
@@ -21,7 +21,7 @@ using std::endl;
 using std::get;
 using Clock = std::chrono::high_resolution_clock;
 
-constexpr std::string_view Version = "dev 1.1.70";
+constexpr std::string_view Version = "dev 1.1.71";
 
 // Evaluation helpers -----------------------------------------------------------------------------
 
