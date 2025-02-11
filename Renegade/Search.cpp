fc50cfb1c2fcd4e64ae10301140cd564e5e8a518
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2025-02-11 20:09:41 +0100

    Bench: 2970430

--- Renegade/Search.cpp
+++ Renegade/Search.cpp
@@ -739,7 +739,6 @@ int Search::SearchQuiescence(ThreadData& t, const int level, int alpha, int beta
 			if (bestScore > alpha) {
 				alpha = bestScore;
 				bestMove = m;
-				scoreType = ScoreType::Exact;
 			}
 		}
 	}
