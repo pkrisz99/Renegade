499dc518f105e06f4fe8b8a8827f5c1017b9fdfc
Krisztián Peőcz <5463243+pkrisz99@users.noreply.github.com>
2025-02-16 18:57:19 +0100

    Organize transposition table entries into clusters (#83)
    Organize entries in the transposition table into clusters of 4, with each being 16 bytes.
    
    Regular STC:
    Elo   | 8.92 +- 4.34 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.93 (-2.25, 2.89) [0.00, 3.50]
    Games | N: 6430 W: 1512 L: 1347 D: 3571
    Penta | [14, 695, 1639, 846, 21]
    https://zzzzz151.pythonanywhere.com/test/1872/
    
    Hash constrained STC:
    Elo   | 13.64 +- 5.45 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=1MB
    LLR   | 2.94 (-2.25, 2.89) [0.00, 3.50]
    Games | N: 4052 W: 982 L: 823 D: 2247
    Penta | [4, 423, 1024, 560, 15]
    https://zzzzz151.pythonanywhere.com/test/1871/
    
    Hash constrained LTC:
    Elo   | 9.72 +- 4.51 (95%)
    SPRT  | 50.0+0.50s Threads=1 Hash=16MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 3.50]
    Games | N: 5614 W: 1279 L: 1122 D: 3213
    Penta | [3, 600, 1447, 751, 6]
    https://zzzzz151.pythonanywhere.com/test/1880/
    
    Unfinished regular LTC:
    Elo   | 2.62 +- 4.20 (95%)
    SPRT  | 50.0+0.50s Threads=1 Hash=128MB
    LLR   | 0.66 (-2.25, 2.89) [0.00, 3.50]
    Games | N: 6508 W: 1417 L: 1368 D: 3723
    Penta | [6, 752, 1692, 795, 9]
    https://zzzzz151.pythonanywhere.com/test/1873/
    
    Renegade dev 1.1.82
    Bench: 3660392

--- Renegade/Transpositions.h
+++ Renegade/Transpositions.h
@@ -6,16 +6,17 @@
 #include <memory>
 
 namespace ScoreType {
-	constexpr int Exact = 0;
-	constexpr int UpperBound = 1; // Alpha (fail-low)
-	constexpr int LowerBound = 2; // Beta (fail-high)
+	constexpr int Invalid = 0;
+	constexpr int Exact = 1;
+	constexpr int UpperBound = 2; // Alpha (fail-low)
+	constexpr int LowerBound = 3; // Beta (fail-high)
 };
 
 struct TranspositionEntry {
 	uint32_t hash = 0;
 	int16_t score = 0;
 	int16_t rawEval = 0;
-	uint16_t quality = 0;
+	uint16_t quality = ScoreType::Invalid;
 	uint8_t depth = 0;
 	uint8_t scoreType = 0;
 	uint16_t packedMove = 0;
@@ -28,14 +29,19 @@ struct TranspositionEntry {
 			|| (scoreType == ScoreType::LowerBound && score >= beta);
 	}
 };
+struct alignas(64) TranspositionCluster {
+	std::array<TranspositionEntry, 4> entries{};
+};
+
 static_assert(sizeof(TranspositionEntry) == 16);
+static_assert(sizeof(TranspositionCluster) == 64);
 
 class Transpositions
 {
 public:
 	Transpositions();
 	void Store(const uint64_t hash, const int depth, const int16_t score, const int scoreType, const int16_t rawEval, const Move& bestMove, const int level, const bool ttPv);
-	bool Probe(const uint64_t& hash, TranspositionEntry& entry, const int level) const;
+	bool Probe(const uint64_t hash, TranspositionEntry& entry, const int level) const;
 	void Prefetch(const uint64_t hash) const;
 	void IncreaseAge();
 	void SetSize(const int megabytes);
@@ -43,8 +49,16 @@ public:
 	int GetHashfull() const;
 
 private:
-	std::vector<TranspositionEntry> Table;
+	std::vector<TranspositionCluster> Table;
 	uint64_t HashMask;
 	uint16_t Age;
+
+	inline uint64_t GetClusterIndex(const uint64_t hash) const {
+		return hash & HashMask;
+	}
+
+	inline uint32_t GetStoredHash(const uint64_t hash) const {
+		return static_cast<uint32_t>((hash & 0xFFFFFFFF00000000) >> 32);
+	}
 };
 
