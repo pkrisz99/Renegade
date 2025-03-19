80add43b1315793237dbade4dba5179393d3ebb8
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2025-03-19 08:25:37 +0100

    Bench: 4604778
    (76k games)

--- Renegade/Histories.h
+++ Renegade/Histories.h
@@ -39,7 +39,7 @@ public:
 private:
 
 	inline void UpdateHistoryValue(int16_t& value, const int amount) {
-		const int gravity = value * std::abs(amount) / 16384;
+		const int gravity = value * std::abs(amount) / 15350;
 		value += amount - gravity;
 	}
 
