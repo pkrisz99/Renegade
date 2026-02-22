fdcdb9c43fadf6c9d4b78bd76b938a6f7d2f1e85
Krisztián Peőcz <5463243+pkrisz99@users.noreply.github.com>
2026-02-22 21:07:03 +0100

    Staged move generation (#155)
    Elo   | 6.31 +- 3.34 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 10676 W: 2559 L: 2365 D: 5752
    Penta | [24, 1183, 2730, 1377, 24]
    
    Renegade dev 1.2.37
    Bench: 4286728

--- Renegade/Movepicker.h
+++ Renegade/Movepicker.h
@@ -7,9 +7,12 @@
 
 
 enum class MovePickerStage {
-	EmitTTMove,
-	GenerateAndScoreMoves,
-	EmitMoves,
+	EmitTTMove = 0,
+	GenerateAndScoreNoisyMoves,
+	EmitGoodNoisyMoves,
+	GenerateAndScoreQuietMoves,
+	EmitQuietMoves,
+	EmitBadNoisyMoves,
 	End
 };
 
@@ -18,41 +21,77 @@ class MovePicker {
 public:
 	MovePicker() = default;
 
-	void Init(const bool onlyNoisyMoves, const Position& pos, const Histories& hist, const Move& ttMove, const int level) {
+	// Sets up the move generation
+	// This is not a constructor as reallocation seems to negatively affect performance.
+	void initialize(const bool skipQuietMoves, const Position& pos, const Histories& hist, const Move& ttMove, const int level) {
 		this->stage = MovePickerStage::EmitTTMove;
 		this->ttMove = ttMove;
 		std::tie(killerMove, counterMove, positionalMove) = hist.GetRefutationMoves(pos, level);
 		this->level = level;
-		this->onlyNoisyMoves = onlyNoisyMoves;
-		this->index = 0;
-		this->moves.clear();
+		this->skipQuietMoves = skipQuietMoves;
+		this->noisyMoveIndex = 0;
+		this->quietMoveIndex = 0;
+		this->noisyMoves.clear();
+		this->quietMoves.clear();
 	}
 
-	// Retrieves the next move
-	std::pair<Move, int> Next(const Position& pos, const Histories& hist) {
+	// Selects the next move (and also returns the order value)
+	// The order is based on information about the position and prior knowledge the engine keeps track of, from most
+	// to least promising. This logic ensures to lazily do the computations we might not need (known as staged movegen).
+	std::pair<Move, int> next(const Position& pos, const Histories& hist) {
 
 		switch (stage) {
 
-		// 1. Return the TT move if possible, skipping move generation entirely
 		case MovePickerStage::EmitTTMove:
-			stage = MovePickerStage::GenerateAndScoreMoves;
+			stage = MovePickerStage::GenerateAndScoreNoisyMoves;
 			if (!ttMove.IsNull() && pos.IsPseudoLegalMove(ttMove) && pos.IsLegalMove(ttMove)) {
 				return { ttMove, 900000 };
 			}
 			[[fallthrough]];
 
-		// 2. Generate moves and score them
-		case MovePickerStage::GenerateAndScoreMoves:
-			pos.GenerateNoisyPseudoLegalMoves(moves);
-			if (!onlyNoisyMoves) pos.GenerateQuietPseudoLegalMoves(moves);
-			for (auto& m : moves) m.orderScore = GetMoveScore(pos, hist, m.move);
-			stage = MovePickerStage::EmitMoves;
+		case MovePickerStage::GenerateAndScoreNoisyMoves:
+			pos.GenerateNoisyPseudoLegalMoves(noisyMoves);
+			for (auto& m : noisyMoves) m.orderScore = getNoisyMoveScore(pos, hist, m.move);
+			stage = MovePickerStage::EmitGoodNoisyMoves;
 			[[fallthrough]];
 
-		// 3. Return moves in decreasing order of merit
-		case MovePickerStage::EmitMoves:
-			while (index < moves.size()) {
-				const auto next = FindNext();
+		case MovePickerStage::EmitGoodNoisyMoves:
+			while (noisyMoveIndex < noisyMoves.size()) {
+				const auto next = findNext(noisyMoves, noisyMoveIndex);
+				if (next.first == ttMove) continue;
+				if (!pos.IsLegalMove(next.first)) continue;
+				if (next.second < -100000) {
+					noisyMoveIndex -= 1;
+					break;
+				}
+				return next;
+			}
+			stage = MovePickerStage::GenerateAndScoreQuietMoves;
+			[[fallthrough]];
+
+		case MovePickerStage::GenerateAndScoreQuietMoves:
+			if (!skipQuietMoves) {
+				pos.GenerateQuietPseudoLegalMoves(quietMoves);
+				for (auto& m : quietMoves) m.orderScore = getQuietMoveScore(pos, hist, m.move);
+				stage = MovePickerStage::EmitQuietMoves;
+			}
+			[[fallthrough]];
+
+		case MovePickerStage::EmitQuietMoves:
+			if (!skipQuietMoves) {
+				while (quietMoveIndex < quietMoves.size()) {
+					const auto next = findNext(quietMoves, quietMoveIndex);
+					if (next.first == ttMove) continue;
+					if (!pos.IsLegalMove(next.first)) continue;
+					return next;
+				}
+			}
+			stage = MovePickerStage::EmitBadNoisyMoves;
+			[[fallthrough]];
+
+		case MovePickerStage::EmitBadNoisyMoves:
+			while (noisyMoveIndex < noisyMoves.size()) {
+				const auto next = findNext(noisyMoves, noisyMoveIndex);
 				if (next.first == ttMove) continue;
 				if (!pos.IsLegalMove(next.first)) continue;
 				return next;
@@ -60,18 +99,18 @@ public:
 			stage = MovePickerStage::End;
 			[[fallthrough]];
 
-		// 4. Signal that all legal moves have been emitted
 		case MovePickerStage::End:
 			return { NullMove, -999999 };
-		
 		}
 	}
 
 	static constexpr int MaxRegularQuietOrder = 200000;
+	bool skipQuietMoves = false;
 
 private:
 
-	std::pair<Move, int> FindNext() {
+	// Selects the move with the next highest score in the list
+	std::pair<Move, int> findNext(MoveList& moves, size_t& index) const {
 		int bestOrderScore = moves[index].orderScore;
 		int bestIndex = index;
 
@@ -87,31 +126,10 @@ private:
 		return { moves[index - 1].move, moves[index - 1].orderScore };
 	}
 
-	int GetMoveScore(const Position& pos, const Histories& hist, const Move& m) const {
+	// Score quiet moves: take the history score and potentially apply a bonus for being a refutation
+	int getQuietMoveScore(const Position& pos, const Histories& hist, const Move& m) const {
 
-		constexpr std::array<int, 7> values = { 0, 100, 300, 300, 500, 900, 0 };
 		const uint8_t movedPiece = pos.GetPieceAt(m.from);
-		const uint8_t capturedPieceType = [&] {
-			if (m.IsCastling()) return PieceType::None;
-			if (m.flag == MoveFlag::EnPassantPerformed) return PieceType::Pawn;
-			return TypeOfPiece(pos.GetPieceAt(m.to));
-		}();
-
-		// Queen promotions
-		if (m.flag == MoveFlag::PromotionToQueen) return 700000 + values[capturedPieceType];
-
-		// Captures
-		if (capturedPieceType != PieceType::None) {
-			const bool losingCapture = [&] {
-				if (onlyNoisyMoves) return false;
-				if (pos.IsMoveQuiet(m)) return false;
-				const int16_t captureScore = (m.IsPromotion()) ? 0 : hist.GetCaptureHistoryScore(pos, m);
-				return !pos.StaticExchangeEval(m, -captureScore / 28);
-			}();
-			return (!losingCapture ? 600000 : -200000) + values[capturedPieceType] * 18 + hist.GetCaptureHistoryScore(pos, m);
-		}
-
-		// Quiet moves: take the history score and potentially apply a bonus for being a refutation
 		int historyScore = hist.GetHistoryScore(pos, m, movedPiece, level);
 
 		if (m == killerMove) historyScore += 17700;
@@ -121,11 +139,29 @@ private:
 		return historyScore;
 	}
 
+	// Score noisy moves: take history, piece types and static exchange eval into account
+	int getNoisyMoveScore(const Position& pos, const Histories& hist, const Move& m) const {
 
-	size_t index = 0;
+		constexpr std::array<int, 7> values = { 0, 100, 300, 300, 500, 900, 0 };
+		const uint8_t capturedPieceType = [&] {
+			if (m.flag == MoveFlag::EnPassantPerformed) return PieceType::Pawn;
+			return TypeOfPiece(pos.GetPieceAt(m.to));
+		}();
+
+		// Queen promotions
+		if (m.flag == MoveFlag::PromotionToQueen) return 700000 + values[capturedPieceType];
+
+		const bool losingCapture = [&] {
+			if (skipQuietMoves) return false;
+			const int16_t captureScore = (m.IsPromotion()) ? 0 : hist.GetCaptureHistoryScore(pos, m);
+			return !pos.StaticExchangeEval(m, -captureScore / 28);
+		}();
+		return (!losingCapture ? 600000 : -200000) + values[capturedPieceType] * 18 + hist.GetCaptureHistoryScore(pos, m);
+	}
+
+	size_t noisyMoveIndex = 0, quietMoveIndex = 0;
 	Move ttMove{}, killerMove{}, counterMove{}, positionalMove{};
-	MoveList moves{};
+	MoveList noisyMoves{}, quietMoves{};
 	int level = 0;
-	bool onlyNoisyMoves = false;
 	MovePickerStage stage = MovePickerStage::EmitTTMove;
 };
