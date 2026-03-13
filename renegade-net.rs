c2d65cf4c16a698db5d1272c9eb31b22bdf7a7cb
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2026-03-13 21:13:19 +0100

    Define net

--- renegade-net.rs
+++ renegade-net.rs
@@ -1,7 +1,7 @@
 /// Training parameters used to create Renegade's networks
-/// bullet version used: e5f65c4 (Measure CPU dataloading throughput utility (#489)) from November 26, 2025
+/// bullet version used: feab644 (Fix wdl scheduler checkpoint loading (#496)) from March 10, 2026
 
-/// Net 35 is trained in 2 stages (keeping the accidental schedule from net 31):
+/// Net 36 is trained in 2 stages (keeping the accidental schedule from net 31):
 /// - first 600 sb: cosine decay, wdl 0.4 -> 0.6
 /// - final 200 sb: continue the cosine decay, wdl fixed at 0.6
 
@@ -31,7 +31,7 @@ use bullet_lib::{
 #[rustfmt::skip]
 fn main() {
     
-    const NET_ID: &str = "renegade_net_35a";
+    const NET_ID: &str = "renegade_net_36a";
     const HL_SIZE: usize = 1600;
     const NUM_OUTPUT_BUCKETS: usize = 8;
     const BUCKET_LAYOUT: [usize; 32] = [
@@ -115,9 +115,21 @@ fn main() {
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
 
-    let data_loader = DirectSequentialDataLoader::new(
-        &["../nnue/data/250418_251202_260112_260217_dfrc260126"]
-    );
+	let data_loader = {
+        let file_path = "251202_260112_260217_260312_dfrc260126.vf";
+        let buffer_size_mb = 2048;
+        let threads = 4;
+		let filter = viriformat::dataformat::Filter {
+            max_eval: 20000,
+            random_fen_skipping: true,
+            random_fen_skip_probability: 0.5,
+            ..Default::default()
+        };
+        ViriBinpackLoader::new(file_path, buffer_size_mb, threads, filter)
+    };
+	// let data_loader = DirectSequentialDataLoader::new(
+    //     &["../nnue/data/file_path"]
+    // );
 
     trainer.run(&schedule, &settings, &data_loader);
     
