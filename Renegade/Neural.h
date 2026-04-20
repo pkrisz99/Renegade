8e3607793b9652a87329b80dc916bc2333c68787
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2026-04-20 17:53:09 +0200

    Bench: 3610228

--- Renegade/Neural.h
+++ Renegade/Neural.h
@@ -20,7 +20,7 @@
 // for each piece: score += (15 - (manhattan distance to opponent's king)) * 6
 
 // Network constants
-#define NETWORK_NAME "renegade-net-35.bin"
+#define NETWORK_NAME "renegade-net-36.bin"
 
 constexpr int FeatureSize = 768;
 constexpr int HiddenSize = 1600;
