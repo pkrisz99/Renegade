0055ffde2ad0e3b26f6abd21c3c185f9cc0fe833
pkrisz99 <5463243+pkrisz99@users.noreply.github.com>
2026-05-08 08:17:54 +0200

    Bench: 4950459

--- Renegade/Search.cpp
+++ Renegade/Search.cpp
@@ -540,7 +540,6 @@ int Search::SearchRecursive(ThreadData& t, int depth, const int level, int alpha
 			if (depth <= 5 && !inCheck && isQuiet && !singularSearch && !IsMateScore(beta) && alpha < MateThreshold && order < 32768 && !position.GivesCheck(m)) {
 				const int futilityMargin = 53 + depth * 100 + improving * 52;
 				if (eval + futilityMargin < alpha) {
-					bestScore = (bestScore + alpha) / 2;
 					break;
 				}
 			}
